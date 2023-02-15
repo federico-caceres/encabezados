93cb9e1ea629a44d8a60e0fd8308518735ba0323
Federico Cáceres <federicoenmanuel@gmail.com>
2023-02-15 19:33:30 -0300

    up

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-//   let info = JSON.stringify('info: ' + req.get('sec-ch-ua'));
-  let info = JSON.stringify(req.headers['user-agent']);
+  let info = JSON.stringify(req.headers["user-agent"]);
   res.send(info);
 });
 
